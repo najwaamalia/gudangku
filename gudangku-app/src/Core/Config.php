8cee06946aee9fe38faa6f7b017c0a5c5341161c
bilarsdka <192354852+bilarsdk@users.noreply.github.com>
2025-12-12 14:51:07 +0700

    Update Config.php

--- gudangku-app/src/Core/Config.php
+++ gudangku-app/src/Core/Config.php
@@ -5,5 +5,5 @@ class Config {
   public const DB_HOST = '127.0.0.1';
   public const DB_NAME = 'gudangku';
   public const DB_USER = 'root';  // Ganti dengan username MySQL Anda
-  public const DB_PASS = '';      // Ganti dengan password MySQL Anda
+  public const DB_PASS = 'admin';      // Ganti dengan password MySQL Anda
 }
